0bd7cb324c593443ab240f069665ff3ee0b6d377
SakaiTaka23 <tt3.sakai@gmail.com>
2020-10-05 14:51:56 +0900

    README.mdの編集

--- pdf_utils/img2txt/forms.py
+++ pdf_utils/img2txt/forms.py
@@ -1,12 +1,26 @@
 from django import forms
 from .models import Image
+from secrets import token_hex
 
 
 class ImageForm(forms.ModelForm):
     image = forms.ImageField(
         widget=forms.ClearableFileInput(attrs={'multiple': True}),
     )
+    task_id = forms.CharField(
+        widget=forms.HiddenInput()
+    )
 
     class Meta:
         model = Image
-        fields = ['image']
+        fields = ['image', 'task_id']
+
+
+def make_task_id():
+    while True:
+        hex = token_hex(10)
+        if Image.object.get(task_id=hex) == '':
+            task_id = hex
+            break
+
+    return task_id
--- pdf_utils/img2txt/models.py
+++ pdf_utils/img2txt/models.py
@@ -8,4 +8,6 @@ class Image(models.Model):
     task_id = models.CharField(unique=True, null=True)
 
     def __str__(self):
-        return self.title
+        return self.task_id
+
+    
